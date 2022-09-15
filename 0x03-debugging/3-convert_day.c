66ce590f5a487b4a58b229c77e839fccba0e99b5
Milad-Rufai <temilader19@gmail.com>
2022-09-15 05:13:22 -0700

    More fix

--- 0x03-debugging/3-convert_day.c
+++ 0x03-debugging/3-convert_day.c
@@ -3,18 +3,17 @@
 
 
 /**
+ * convert_day - converts day of month to day of year, without accounting
  *
- * * convert_day - converts day of month to day of year, without accounting
+ * for leap year
  *
- * * for leap year
+ * @month: month in number format
  *
- * * @month: month in number format
+ * @day: day of month
  *
- * * @day: day of month
+ * Return: day of year
  *
- * * Return: day of year
- *
- * */
+ */
 
 
 
--- 0x03-debugging/3-main_a.c
+++ 0x03-debugging/3-main_a.c
@@ -5,14 +5,13 @@
 
 
 /**
+ * main - takes a date and prints how many days are left in the year, taking
  *
- * * main - takes a date and prints how many days are left in the year, taking
+ * leap years into account
  *
- * * leap years into account
+ * Return: 0
  *
- * * Return: 0
- *
- * */
+ */
 
 
 
