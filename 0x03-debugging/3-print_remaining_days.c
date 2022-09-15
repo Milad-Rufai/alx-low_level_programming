2771cdc03406ffd321b9fd14a8c2d5fc93f2451a
Milad-Rufai <temilader19@gmail.com>
2022-09-15 06:11:30 -0700

    More fix

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -5,20 +5,19 @@
 
 
 /**
+ * print_remaining_days - takes a date and prints how many days are
  *
- * * print_remaining_days - takes a date and prints how many days are
+ * left in the year, taking leap years into account
  *
- * * left in the year, taking leap years into account
+ * @month: month in number format
  *
- * * @month: month in number format
+ * @day: day of month
  *
- * * @day: day of month
+ * @year: year
  *
- * * @year: year
+ * Return: void
  *
- * * Return: void
- *
- * */
+ */
 
 
 
